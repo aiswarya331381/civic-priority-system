adfffda9a8561aea9cd1dd512835fd1a78c5e503
aiswarya331381 <aiswarya331381@gmail.com>
2026-06-19 16:35:59 +0530

    Fix phone number registration

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://civic-backend-dgmu.onrender.com/api'
+ baseURL: 'https://civic-backend-dgmu.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
